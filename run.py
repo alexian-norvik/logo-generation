74ff4a16d3637527ed4a840ec8e7abba11fd38bd
norvik <ai.norvik@gmail.com>
2025-08-23 21:44:49 +0400

    created run module to execute the logo generation functionality

---
+++ run.py
@@ -0,0 +1,7 @@
+from services import logo_generator
+
+prompt = """
+High quality Minimalistic pastry logo called: Cakey_EVN in dark color, using pastel orange, pink, and use white as background color. Do not include any other text, use font style that goes with the pastry style.
+"""
+
+logo_generator.generate_logo(prompt=prompt)
